87e20befbf85dd81d81f2dd722212edc855ccfb2
YusefAli <yusef.ali.garcia@gmail.com>
2015-08-04 03:31:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ ss.http.route('/', function(req, res){
 ss.client.templateEngine.use(require('ss-hogan'));
 
 ss.publish.transport.use('redis',{host: 'pub-redis-15488.us-west-2-1.1.ec2.garantiadata.com', port: 15488, pass: 'Orezipal1'});
+ss.session.store.use('redis',{host: 'pub-redis-15488.us-west-2-1.1.ec2.garantiadata.com', port: 15488, pass: 'Orezipal1'});
 
 // Minimize and pack assets if you type: SS_ENV=production node app.js
 if (ss.env === 'production') ss.client.packAssets();
